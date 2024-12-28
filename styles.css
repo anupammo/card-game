3dd0705b59139fcdbb15989fbcd5e4c81a109917
Anupam Mondal <a09051985@gmail.com>
2024-12-29 00:32:34 +0530

    Create styles.css

---
+++ styles.css
@@ -0,0 +1,33 @@
+body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background-color: #2e3a40;
+    font-family: Arial, sans-serif;
+    margin: 0;
+}
+
+.game-board {
+    display: grid;
+    grid-template-columns: repeat(4, 100px);
+    grid-gap: 10px;
+}
+
+.card {
+    width: 100px;
+    height: 150px;
+    background-color: #fff;
+    border: 2px solid #444;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 24px;
+    color: #444;
+}
+
+.card.flipped {
+    background-color: #ffeb3b;
+    color: #000;
+}
